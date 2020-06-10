9026c78afa0e7f9ac9a26587cce38a43754943c9
RenatiusvanSander / R. Meier <remad@web.de>
2020-06-10 18:57:08 +0200

    chapter-02-junit-annotations: Standard tests

---
+++ src/test/java/edu/remad/learnjunit5/StandardTests.java
@@ -0,0 +1,49 @@
+package edu.remad.learnjunit5;
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
+
+/**
+ * This class has standard tests using JUnit5.
+ */
+public class StandardTests {
+
+    @BeforeAll
+    static void initAll() {
+    }
+
+    @BeforeEach
+    void init() {
+    }
+
+    @Test
+    void succeedingTest() {
+    }
+
+    @Test
+    void failingTest() {
+        fail("a failing test");
+    }
+
+    @Test
+    @Disabled("for demonstration purposes")
+    void skippedTest() {
+        // not executed
+    }
+
+    @Test
+    void abortedTest() {
+        assumeTrue("abc".contains("Z"));
+        fail("test should have been aborted");
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+    }
+}
